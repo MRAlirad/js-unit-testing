c5843ac88f8d55bda0f27cc7bbbcfb3a6f3fecdc
MRAlirad <m.r.alirad75@gmail.com>
2024-03-04 22:37:43 +0330

    Intro : FizzBuzz method unit testing

--- test/Intro.test.js
+++ test/Intro.test.js
@@ -1,5 +1,5 @@
 import {describe, test, it, expect} from 'vitest';
-import {max} from '../src/intro';
+import {max, fizzBuzz} from '../src/intro';
 
 
 // 'describe method' => create a test suite or a group of related tests
@@ -33,7 +33,23 @@ describe('max', () => {
     it('should return the second argument if it is greater', () => {
         expect(max(1, 2)).toBe(2);
     });
+
     it('should return the first argument if arguments are equal', () => {
         expect(max(1, 1)).toBe(1);
+    });
+});
+
+describe('fizzBuzz', () => {
+    it('should return FizzBuzz if number is divisible by 3 and 5', () => {
+        expect(fizzBuzz(15)).toBe('FizzBuzz');
+    });
+    it('should return Fizz if number is divisible by 3', () => {
+        expect(fizzBuzz(9)).toBe('Fizz');
+    });
+    it('should return Buzz if number is divisible by 5', () => {
+        expect(fizzBuzz(5)).toBe('Buzz');
+    });
+    it('should return the string type of number if number is not divisible by 5 nor 3', () => {
+        expect(fizzBuzz(2)).toBe('2');
     });
 });
