3457fa15d54ea742012fa66b073f00a8390f5e96
MRAlirad <m.r.alirad75@gmail.com>
2024-03-08 14:11:13 +0330

    Testing getCoupons
    Core Unit Testing Techniques
    Writing good Assertions
    Testing getCoupons Exercise

--- test/core.test.js
+++ test/core.test.js
@@ -1,4 +1,5 @@
 import {it, expect, describe} from 'vitest';
+import {getCoupons} from '../src/core';
 
 describe('test suite', () => {
     it('test case', () => {
@@ -8,7 +9,7 @@ describe('test suite', () => {
 });
 
 describe('writing Good Assertion for strings', () => {
-    it('error test', ()=> {
+    it('error test', () => {
         const result = 'The requested file was not found.';
 
         // Loose(too general)
@@ -16,15 +17,14 @@ describe('writing Good Assertion for strings', () => {
 
         // Tight (too specific)
         expect(result).toBe('The requested file was not found.');
-        
+
         // check for the presence of specific keywords or phrases in this sring
         expect(result).toMatch(/not found/i);
-    })
-    
+    });
 });
 
 describe('writing Good Assertion for arrays', () => {
-    it('error test', ()=> {
+    it('arrays test', () => {
         const result = [1, 2, 3];
 
         // Loose(too general)
@@ -37,11 +37,11 @@ describe('writing Good Assertion for arrays', () => {
 
         // Better assertion
         expect(result.length).toBeGreaterThan(0);
-    })
+    });
 });
 
 describe('writing Good Assertion for object', () => {
-    it('error test', ()=> {
+    it('object test', () => {
         const result = {name: 'Mohammad'};
 
         // Loose(too general)
@@ -53,5 +53,32 @@ describe('writing Good Assertion for object', () => {
         // Better assertion
         expect(result).toHaveProperty('name');
         expect(typeof result.name).toBe('string');
-    })
+    });
+});
+
+describe('getCoupons', () => {
+    it('should return an array of coupons', () => {
+        const coupons = getCoupons();
+        expect(Array.isArray(coupons)).toBe(true);
+        expect(coupons.length).toBeGreaterThan(0);
+    });
+
+    it('should return an array with valid coupon codes', () => {
+        const coupons = getCoupons();
+        coupons.forEach(coupon => {
+            expect(coupon).toHaveProperty('code');
+            expect(typeof coupon.code).toBe('string');
+            expect(coupon.code.length).toBeTruthy();
+        });
+    });
+    it('should return an array with valid discounts', () => {
+        const coupons = getCoupons();
+        coupons.forEach(coupon => {
+            expect(coupon).toHaveProperty('discount');
+            expect(typeof coupon.discount).toBe('number');
+            expect(coupon.code).toBeTruthy();
+            expect(coupon.discount).toBeGreaterThan(0);
+            expect(coupon.discount).toBeLessThan(1);
+        });
+    });
 });
